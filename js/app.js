98a4747d627bd07e1aa2fe2fc4a431791998abcc
alexyuu <alex.cm.yu@gmail.com>
2016-04-12 20:42:20 -0700

    update home page to single line

--- js/app.js
+++ js/app.js
@@ -1,36 +1,65 @@
 var titles = [ 'front-end wizard', 'ad tech ninja', 'kung-fu master', 'hardcore gamer', 'dog lover', 'hard-ish worker', 'freestyle dancer', 'Chandler Bing', 'times ten equals five', 'Golden State Warrior', 'Triton',  ]
+var init = 0
 
 $(document).ready(function(){
+event.preventDefault()
 
-// defining var in array
+
+
   var randomNumber = function(prev) {
-    var next = Math.floor(Math.random() * titles.length);
-    while(prev === next) {
-      next = Math.floor(Math.random() * titles.length);
-    }
-    return next;
+      var next = Math.floor(Math.random() * titles.length);
+      while(prev === next) {
+        next = Math.floor(Math.random() * titles.length);
+      }
+      return next;
   }
 
-// initial
-  var adj1 = 0,
-    adj2 = 1;
 
-// Loop
-  setInterval(function() {
-    adj1 = randomNumber(adj1);
-    adj2 = randomNumber(adj2);
-
-    while(adj1 === adj2) {
-      adj1 = randomNumber(adj1);
-      adj2 = randomNumber(adj2);
+  setInterval(function(){
+    if (init == 0 || init > titles.length) {
+      init = 0;
+      $('#adjOne').text(titles[init]);
+      init = init + 1;
+    } else {
+      init = randomNumber();
+      $('#adjOne').text(titles[init]);
     }
+  }, 3000);
 
-    $('#adjOne').text(titles[adj1]);
-    $('#adjTwo').text(titles[adj2]);
-  }, 3000)
+  $('#adjOne').text(titles[init]);
 
-// initial
-  $('#adjOne').text(titles[adj1]);
-  $('#adjTwo').text(titles[adj2]);
+})
 
-});
+//
+// // defining var in array
+//   var randomNumber = function(prev) {
+//     var next = Math.floor(Math.random() * titles.length);
+//     while(prev === next) {
+//       next = Math.floor(Math.random() * titles.length);
+//     }
+//     return next;
+//   }
+//
+// // initial
+//   var adj1 = 0,
+//     // adj2 = 1;
+//
+// // Loop
+//   setInterval(function() {
+//     adj1 = randomNumber(adj1);
+//   //   adj2 = randomNumber(adj2);
+//   //
+//   //   while(adj1 === adj2) {
+//   //     adj1 = randomNumber(adj1);
+//   //     adj2 = randomNumber(adj2);
+//   //   }
+//
+//     $('#adjOne').text(titles[adj1]);
+//     // $('#adjTwo').text(titles[adj2]);
+//   }, 3000)
+//
+// // initial
+//   $('#adjOne').text(titles[adj1]);
+//   // $('#adjTwo').text(titles[adj2]);
+//
+// });
