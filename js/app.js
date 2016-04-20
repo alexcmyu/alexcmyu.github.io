1ef28f78538e3c6591c3900212e22d357ccd3eb4
alexyuu <alex.cm.yu@gmail.com>
2016-04-19 18:28:07 -0700

    adding other divs

--- js/app.js
+++ js/app.js
@@ -1,22 +1,65 @@
 var titles = [ 'front-end wizard', 'ad tech ninja', 'kung-fu master', 'hardcore gamer', 'dog lover', 'hard-ish worker', 'freestyle dancer', 'Chandler Bing', 'Golden State Warrior', 'Triton',  ]
 var currentTitle = 0
-var lastTitle
 
 $(document).ready(function() {
 event.preventDefault()
 
-  var pickRandomNumber = function() {
-      return Math.floor(Math.random() * titles.length);
+// For array to randomly select
+//   var pickRandomNumber = function(prev) {
+//       num = Math.floor(Math.random() * titles.length);
+//       if (num === prev) {
+//       	pickRandomNumber(num);
+//       } else {
+//       	return num;
+//       }
+//   }
+//
+//   setInterval(function() {
+//     currentTitle = pickRandomNumber(lastTitle)
+//     lastTitle = currentTitle
+//     $('#adjOne').text(titles[currentTitle]);
+//   }, 3000);
+// })
+
+
+setInterval(function(){
+  if(currentTitle === titles.length){
+    currentTitle = 0
+    // console.log('zero')
+    $('#adjOne').text(titles[currentTitle]);
+    currentTitle = currentTitle + 1
+  } else {
+    // console.log('else')
+    $('#adjOne').text(titles[currentTitle]);
+    currentTitle = currentTitle + 1
   }
 
-  setInterval(function() {
-    currentTitle = pickRandomNumber()
+}, 3000);
 
-    // while (currentTitle === lastTitle){
-    //   pickRandomNumber()
-    // }
 
-    $('#adjOne').text(titles[currentTitle]);
-
-  }, 3000);
+$('.aboutmeLink').click(function(){
+  $('#aboutme').css({
+    'display':'block',
+  })
 })
+
+$('.aboutmeLink').click(function(){
+  $('#home').css({
+    'display':'none',
+  })
+})
+
+$('.workLink').click(function(){
+  $('#mywork').css({
+    'display':'block',
+  })
+})
+
+$('.workLink').click(function(){
+  $('#home').css({
+    'display':'none',
+  })
+})
+
+
+});
