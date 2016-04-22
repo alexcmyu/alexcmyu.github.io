d6b34b9b3b466157051cc6d854b7b1e40ee305cb
alexyuu <alex.cm.yu@gmail.com>
2016-04-21 19:07:46 -0700

    hide nav buttons
    need to figure out how to have them display once aboutme and work divs
    display

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var titles = [ 'front-end wizard', 'ad tech ninja', 'kung-fu master', 'hardcore gamer', 'dog lover', 'hard-ish worker', 'freestyle dancer', 'Chandler Bing', 'Golden State Warrior', 'Triton',  ]
+var titles = [ 'ad tech ninja', 'kung-fu master', 'hardcore gamer', 'dog lover', 'hard-ish worker', 'front-end wizard', 'freestyle dancer', 'Chandler Bing', 'Golden State Warrior', 'Triton',  ]
 var currentTitle = 0
 
 $(document).ready(function() {
@@ -35,19 +35,74 @@ event.preventDefault()
 
 
 
-  // Nav links, shifting divs
+// Nav links, shifting divs
+  // top about me link
   $(document).ready(function(){
     $('.aboutmeLink').on('click', function(){
-        $('#home').animate({'margin-top': '90vh'}, 1000);
+      $('#home').animate({'top': '90vh'}, 1000);
     });
   });
 
+  // top home link
   $(document).ready(function(){
-    $('.workLink').on('click', function(){
-        $('#aboutme').css('display', 'none');
-        $('#home').animate({'top': '-90vh'}, 1000);
+    $('.homeLink-top').on('click', function(){
+      $('#home').animate({'top': '0vh'}, 1000);
     });
   });
+
+  // top work link
+  $(document).ready(function(){
+    $('.workLink-top').on('click', function(){
+      $('#home').animate({'top': '-90vh'}, 800);
+      $('#aboutme').css('display', 'none'); // need to figure out delay, fade?
+    });
+  });
+
+  // bottom work link
+  $(document).ready(function(){
+    $('.workLink').on('click', function(){
+      $('#aboutme').css('display', 'none');
+      $('#home').animate({'top': '-90vh'}, 1000);
+    });
+  });
+
+  // bottom home link
+  $(document).ready(function(){
+    $('.homeLink-bottom').on('click', function(){
+      $('#aboutme').css('display', 'block'); // might need delay too
+      $('#home').animate({'top': '0vh'}, 1000);
+    });
+  });
+
+  // bottom about me link
+  $(document).ready(function(){
+    $('.aboutmeLink-bottom').on('click', function(){
+      $('#aboutme').css('display', 'block'); // might need delay too
+      $('#home').animate({'top': '90vh'}, 800);
+    });
+  });
+// end nav links
+
+  // $('#aboutme').visible('diplay', 'block');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 });
